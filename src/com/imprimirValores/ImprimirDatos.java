bd08e7f6b1a52160d7488ce4b4f59f4f6b481811
Gescientist-bit <geosof03@gmail.com>
2022-09-29 11:56:26 -0500

    tipo de datos java basico

---
+++ src/com/imprimirValores/ImprimirDatos.java
@@ -0,0 +1,16 @@
+package com.imprimirValores;
+
+public class ImprimirDatos {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        long numeroLong = 100;
+        double numeroDouble = 1528d;
+        boolean numeroBoolean = true;
+        String datoString = "Hola mundo";
+        System.out.println("numeroInt --> " + numero + "\n" + "numero long --> " + numeroLong
+                + "\n" + "numeroDouble --> " + numeroDouble + "\n" + "numeroBoolean --> " + numeroBoolean
+                + "\n" + "datoString --> " + datoString);
+    }
+}
